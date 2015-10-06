e143987a01c3d5b59452d39c99119db26d84bf31
sashakmets <sashakmets@yandex.ru>
2015-10-06 03:48:07 +0300

    master

---
+++ src/main/java/com/instanceofjava/A3.java
@@ -0,0 +1,18 @@
+package main.java.com.instanceofjava;
+
+/**
+ * Created by Саша on 06.10.2015.
+ */
+public class A3 {
+
+    public static void main(String[] args)  {
+        Integer i1 = 128;
+        Integer i2 = 128;
+        System.out.println(i1 == i2);
+
+        Integer i3 = 127;
+        Integer i4 = 127;
+        System.out.println(i3 == i4);
+    }
+
+}
